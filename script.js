1364475ac80ab093c01d08d19fae0cd985a4c7fe
RishabhSahil <91429171+RishabhSahil@users.noreply.github.com>
2021-12-01 21:48:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
 document.getElementById('frmsearch').onsubmit = function() {
     window.location = 'search.html?q=' + document.getElementById('textSearch').value;
-    return false;
+    query = document.getElementById('textSearch').value;
+    if(query==false){
+        // alert("Please Enter Your Query")
+        console.log(query)
+    }
+    else{
+        window.location = 'search.html?q=' + query
+        return false;
+
+    }
 
 }
