d8bb4cf7c8e6db6a4568658d4384a0233ff4efd2
RishabhSahil <91429171+RishabhSahil@users.noreply.github.com>
2021-12-12 17:22:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-document.getElementById('frmsearch').onsubmit = function() {
-    query = document.getElementById('textSearch').value;
-    if(query==false){
-        // alert("Please Enter Your Query")
-        console.log("please enter your query");
-        return false
-    }
-
-    else{
-        window.location = 'search.html?q=' + query
-        return false;
-
-    }
-
-}
-
 function wikipediasearch(){
     query = document.getElementById('textSearch').value;
     if(query==false){
@@ -136,3 +120,19 @@ function songmq(){
 
     }
 } 
+
+document.getElementById('frmsearch').onsubmit = function() {
+    query = document.getElementById('textSearch').value;
+    if(query==false){
+        // alert("Please Enter Your Query")
+        console.log("please enter your query");
+        return false
+    }
+
+    else{
+        window.location = 'search.html?q=' + query
+        return false;
+
+    }
+
+}
